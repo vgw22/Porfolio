fe33ebb492c3b9ec049714a5b7b1dd8e9e9ec8ef
vgw22 <vitorgabrielwinck2222@gmail.com>
2024-07-01 19:26:50 -0300

    Deleted htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
-const htmlmin = require('gulp-htmlmin');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
@@ -15,16 +14,9 @@ function compileStyles() {
         .pipe(gulp.dest('./dist/styles'))
 }
 
-function minifyHtml() {
-    return gulp.src('./src/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('./dist'))
-}
-
-exports.default = gulp.parallel(compileStyles, scripts, minifyHtml);
+exports.default = gulp.parallel(compileStyles, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(compileStyles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-    gulp.watch('./src/*.html'), gulp.parallel(minifyHtml)
 }
